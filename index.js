d05d981e11397bab7fe675b53b2e586498ad1c50
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-26 13:02:06 -0400

    Imported basic endpoint scaffolding

--- index.js
+++ index.js
@@ -11,6 +11,31 @@ server.use(helmet());
 
 // endpoints here
 
+server.get('/', (req, res) => {
+  // get the roles from the database
+  res.send('Write code to retrieve all roles');
+});
+
+server.get('/:id', (req, res) => {
+  // retrieve a role by id
+  res.send('Write code to retrieve a role by id');
+});
+
+server.post('/', (req, res) => {
+  // add a role to the database
+  res.send('Write code to add a role');
+});
+
+server.put('/:id', (req, res) => {
+  // update roles
+  res.send('Write code to modify a role');
+});
+
+server.delete('/:id', (req, res) => {
+  // remove roles (inactivate the role)
+  res.send('Write code to remove a role');
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
