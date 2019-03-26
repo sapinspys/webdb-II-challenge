fda08363983fed7f99496d048c37c0da003aaafc
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-26 13:45:48 -0400

    GET by id now functional

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const helmet = require('helmet');
-const knex = require('knex');
-const knexConfig = require('./knexfile');
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
+const knexConfig = require("./knexfile");
 
 const db = knex(knexConfig.development);
 
@@ -12,47 +12,56 @@ server.use(helmet());
 
 // endpoints here
 
-server.get('/api/zoos', (req, res) => {
+server.get("/api/zoos", (req, res) => {
   // get the zoos from the database
-  db('zoos')
-  .then(zoos => {
-    res.status(200).json(zoos);
-  })
-  .catch(error => {
-    res.status(500).json(error);
-  })
-});
-
-server.get('/:id', (req, res) => {
-  // retrieve a role by id
-  res.send('Write code to retrieve a role by id');
-});
-
-server.post('/api/zoos', (req, res) => {
-  const newZoo = req.body;
-
-  if (newZoo.name) {
-    db('zoos')
-    .insert(newZoo)
-    .then(id => {
-      res.status(201).json(id);
+  db("zoos")
+    .then(zoos => {
+      res.status(200).json(zoos);
     })
     .catch(error => {
       res.status(500).json(error);
+    });
+});
+
+server.get("/api/zoos/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("zoos")
+    .where({ id })
+    .first() // allows you to get out of array
+    .then(zoos => {
+      res.status(200).json(zoos);
     })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+server.post("/api/zoos", (req, res) => {
+  const newZoo = req.body;
+
+  if (newZoo.name) {
+    db("zoos")
+      .insert(newZoo)
+      .then(id => {
+        res.status(201).json(id);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
   } else {
-    res.status(400).json({ error: "Please provide a name for the zoo." })
+    res.status(400).json({ error: "Please provide a name for the zoo." });
   }
 });
 
-server.put('/:id', (req, res) => {
+server.put("/:id", (req, res) => {
   // update roles
-  res.send('Write code to modify a role');
+  res.send("Write code to modify a role");
 });
 
-server.delete('/:id', (req, res) => {
+server.delete("/:id", (req, res) => {
   // remove roles (inactivate the role)
-  res.send('Write code to remove a role');
+  res.send("Write code to remove a role");
 });
 
 const port = 3300;
