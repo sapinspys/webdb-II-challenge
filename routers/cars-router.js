d886e06bb28a07c9d0e9d6d09619b62b1a4b6ac3
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-07-16 12:56:25 -0400

    Cars router created and popualted

---
+++ routers/cars-router.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+// Knex configuration
+const knex = require('knex');
+const knexConfig = require('../knexfile');
+const db = knex(knexConfig.development);
+
+const router = express.Router();
+
+router.post("/", (req, res) => {
+  const newVehicle = req.body;
+
+  if (newVehicle['VIN'] && newVehicle.make && newVehicle.model && newVehicle.milage) {
+    db('cars')
+      .insert(newVehicle)
+      .then(test => {
+        res.status(201).json(test)
+      })
+      .catch(error => {
+        res.status(500).json(error)
+      })
+  } else {
+    res.status(400).json({ error: "Vehicle VIN, make, model, and milage are required fields."})
+  }
+})
+
+router.get("/", (req, res) => {
+  db('cars')
+    .then(cars => {
+      res.status(200).json(cars)
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+}) 
